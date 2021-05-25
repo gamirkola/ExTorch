59f5a0ecf3ece214efeb90fe5f0364735e1dc071
gamirkola <mirkogal@gmail.com>
2021-05-25 23:34:36 +0200

    added cache screenshot function

--- app/src/main/java/com/mlw/extorch/MainActivity.java
+++ app/src/main/java/com/mlw/extorch/MainActivity.java
@@ -2,15 +2,27 @@ package com.mlw.extorch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.NotDirectoryException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // work around for uri exposed
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
 
         flashControl = findViewById(R.id.flashSwitch);
         cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
@@ -47,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                     } catch (CameraAccessException e) {
                         e.printStackTrace();
                     }
+                    screenshoot();
                     flashControl.setText("Flash OFF");
                 }else{
                     try {
@@ -59,6 +76,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void screenshoot(){
+        Date date = new Date();
+        CharSequence now = android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", date);
+        //create file directory in cache app dir
+        File temp_dir = new File(getCacheDir(), "scrn");
+        temp_dir.mkdir();
+
+        String scrn_name = getCacheDir() + "/scrn/" + now + ".jpg";
+
+        View root = getWindow().getDecorView();
+        root.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(root.getDrawingCache());
+        root.setDrawingCacheEnabled(false);
+
+        File scrn = new File(scrn_name);
+        try {
+            scrn.createNewFile();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(scrn);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            // image show not needed, but no working anyway :(
+//            Uri uri = Uri.fromFile(scrn);
+//            Intent intent = new Intent(Intent.ACTION_VIEW);
+//            intent.setDataAndType(uri, "image/*");
+//            startActivity(intent);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
