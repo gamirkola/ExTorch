fbc22f1bfb07b7b9e3b8f243126f67d38c930a62
gamirkola <mirkogal@gmail.com>
2021-05-25 17:11:15 +0200

    basic camera flash activation

--- app/src/main/java/com/mlw/extorch/MainActivity.java
+++ app/src/main/java/com/mlw/extorch/MainActivity.java
@@ -2,18 +2,62 @@ package com.mlw.extorch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch flashControl;
+    CameraManager cameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void OnOffTorch(View view){
+        flashControl = findViewById(R.id.flashSwitch);
+        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+
+        //WE NEED TO CHECK IF THE DEVICE HAS A CAMERA (OR FLASH)
+        if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)){
+            if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+                //HERE WE TURN THE FLASH ON AND START THE EXFILL
+                flashControl.setEnabled(true);
+            }else{
+                Toast.makeText(MainActivity.this, "This device has no flash!", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(MainActivity.this, "This device has no camera!", Toast.LENGTH_SHORT).show();
+        }
+
+        flashControl.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //user turned on thew flashlight, the default rear camera has 0 as id
+                    try {
+                        cameraManager.setTorchMode("0", false);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                    flashControl.setText("Flash ON");
+                }else{
+                    try {
+                        cameraManager.setTorchMode("0", true);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                    flashControl.setText("Flash OFF");
+                }
+            }
+        });
 
     }
 
