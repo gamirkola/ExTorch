460b064cd00bb4657885b1e0d63e9473133aacaf
gamirkola <mirkogal@gmail.com>
2021-05-31 23:00:15 +0200

    little fix

--- app/src/main/java/com/mlw/extorch/MainActivity.java
+++ app/src/main/java/com/mlw/extorch/MainActivity.java
@@ -35,11 +35,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Date;
 
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity {
 
     Switch flashControl;
